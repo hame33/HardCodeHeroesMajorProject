9103bedc94ff938c41e0142b459947f1600d06d1
CaseTarsierTheFirst <160694517+CaseTarsierTheFirst@users.noreply.github.com>
2024-10-23 21:27:39 +1100

    added build folder

---
+++ shape_detector2/build/shape_detector/CMakeFiles/shape_detector_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shape_detector_node.dir/src/shape_detector.cpp.o"
+  "CMakeFiles/shape_detector_node.dir/src/shape_detector.cpp.o.d"
+  "shape_detector_node"
+  "shape_detector_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shape_detector_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
