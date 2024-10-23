08b91a64cddbd14f29ef002deb0d47eff054f5f7
CaseTarsierTheFirst <160694517+CaseTarsierTheFirst@users.noreply.github.com>
2024-10-23 15:23:34 +1100

    Added build folder
    added the build folder

---
+++ shape_detector/build/shape_detector/CMakeFiles/shape_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shape_detector.dir/src/shape_detector.cpp.o"
+  "CMakeFiles/shape_detector.dir/src/shape_detector.cpp.o.d"
+  "shape_detector"
+  "shape_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shape_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
