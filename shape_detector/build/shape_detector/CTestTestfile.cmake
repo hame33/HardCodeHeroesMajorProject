0f1e09c0770b22b0e02494981e21dfd9cf556c6f
CaseTarsierTheFirst <160694517+CaseTarsierTheFirst@users.noreply.github.com>
2024-10-23 15:28:02 +1100

    added build folder
    added build folder

---
+++ shape_detector/build/shape_detector/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peter/turtlebot3_ws/src/shape_detector
+# Build directory: /home/peter/turtlebot3_ws/src/shape_detector/build/shape_detector
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
