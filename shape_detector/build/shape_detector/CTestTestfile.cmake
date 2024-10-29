da596b1f0d91a12509dca651ba525dae63774bfb
CaseTarsierTheFirst <160694517+CaseTarsierTheFirst@users.noreply.github.com>
2024-10-29 23:05:28 +1100

    Delete shape_detector/build directory
    remove the build folder which is unused

--- shape_detector/build/shape_detector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/turtlebot3_ws/src/shape_detector
-# Build directory: /home/peter/turtlebot3_ws/src/shape_detector/build/shape_detector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
