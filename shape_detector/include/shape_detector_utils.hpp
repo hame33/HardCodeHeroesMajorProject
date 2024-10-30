d87cc9a4288f5f1536964088e4dd57cc1ba21aa0
lucaagius3 <160693599+lucaagius3@users.noreply.github.com>
2024-10-30 21:45:23 +1100

    Update shape_detector_utils.hpp to OOD

--- shape_detector/include/shape_detector_utils.hpp
+++ shape_detector/include/shape_detector_utils.hpp
@@ -1,13 +1,34 @@
-// shape_detector_utils.hpp
-
 #ifndef SHAPE_DETECTOR_UTILS_HPP
 #define SHAPE_DETECTOR_UTILS_HPP
 
+// --- Includes ---
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <string>
 
-// Enum for Shape Types
-enum class ShapeType {
+// --- ShapeDetectorUtils Class Interface ---
+// The ShapeDetectorUtils class provides utilities for detecting shapes and colors 
+// within an image. It includes methods for determining the type of shape based 
+// on contour points and detecting the color based on HSV values.
+
+class ShapeDetectorUtils 
+{
+public:
+  // Constructor - Default constructor for ShapeDetectorUtils
+  ShapeDetectorUtils();
+
+  // --- ShapeDetectorUtils Functions ---
+
+  // detect_shape - Detects shape type from contour and outputs the shape's name
+  size_t detect_shape(const std::vector<cv::Point>& contour, std::string &shape);
+
+  // detect_color - Detects color based on HSV values from the frame and contour area
+  std::string detect_color(const cv::Mat &frame, const std::vector<cv::Point> &contour);
+
+  // --- Enums ---
+  // Enumeration defining various shape types by vertex count
+  enum class ShapeType 
+  {
     TRIANGLE = 3,
     SQUARE = 4,
     RECTANGLE = 5,
@@ -19,10 +40,11 @@ enum class ShapeType {
     DECAGON = 10,
     UNIDENTIFIED = 11,
     CIRCLE = 14
-};
+  };
 
-// Enum for Color Ranges
-enum class ColorRange {
+  // Enumeration defining HSV ranges for color detection
+  enum class ColorRange 
+  {
     RED_MIN = 0,
     RED_MAX = 10,
     ORANGE_MIN = 11,
@@ -39,10 +61,7 @@ enum class ColorRange {
     PINK_MAX = 180,
     WHITE_MIN = 181,
     WHITE_MAX = 200
+  };
 };
 
-// Function declarations
-size_t detect_shape(const std::vector<cv::Point>& contour, std::string &shape);
-std::string detect_color(const cv::Mat &frame, const std::vector<cv::Point> &contour);
-
-#endif // SHAPE_DETECTOR_UTILS_HPP
+#endif  // SHAPE_DETECTOR_UTILS_HPP
